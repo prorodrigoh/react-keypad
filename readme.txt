5e90cab9180178ffb9d851c38cafa4f0249234ee
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-05-13 14:45:10 -0400

    working

---
+++ readme.txt
@@ -0,0 +1,18 @@
+create a keypad with react
+blank screen on top 
+keypad 
+1  2  3
+4  5  6
+7  8  9
+←  0  ✓
+as key in a number
+puts the number on the top blank screen
+back button erases the last entry
+valid button clears content
+
+try if 'hello'.length = 4 returns 'hell'
+
+bonus
+
+onClick change colors
+onClick valid button, tell if code is valid or error
--- src/App.js
+++ src/App.js
@@ -1,22 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let displayMsg = 'Enter Code 102938'
+  const [displayLen, setDisplayLen] = useState(0)
+  const [displayNum, setDisplayNum] = useState(displayMsg)
+  const [btnCheckPressed, setBtnCheckPressed] = useState(false)
+
+  function numericBtn (i){
+    if (displayLen < 6) {
+      if(btnCheckPressed){
+        setBtnCheckPressed(!btnCheckPressed)
+        resetDisplay(i)
+        return
+      }
+
+      if(displayLen===0){
+        setDisplayNum(i)
+      }else{
+        setDisplayNum(displayNum + i)
+      }
+
+      setDisplayLen(displayLen + 1)
+    }
+  }
+
+  function resetDisplay (j){
+    setDisplayNum(displayMsg)
+    setDisplayLen(0)
+    numericBtn(j)
+    return
+  }
+
+
+  function backBtn (){
+    if(!btnCheckPressed){
+      let newDisplayNum = displayNum
+      if(displayLen>0){
+        setDisplayNum(newDisplayNum.slice(0,displayLen - 1))
+        setDisplayLen(displayLen - 1)
+      }
+    }
+  }
+
+  function checkBtn (){
+    if(!btnCheckPressed){
+      if(displayNum==='102938'){
+        setDisplayNum('Code Valid')
+      }else{
+        setDisplayNum('Invalid Code')
+      }
+      setDisplayLen(0)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <section className='displayRow'>
+          <h2>{displayNum}</h2>
+        </section>
+
+        <section className='firstRow'>
+          <button type="button" className='btn btn-primary' id='btn-1' onClick={()=> numericBtn('1')}>1</button>
+          <button type="button" className='btn btn-primary' id='btn-2' onClick={()=> numericBtn('2')}>2</button>
+          <button type="button" className='btn btn-primary' id='btn-3' onClick={()=> numericBtn('3')}>3</button>
+        </section>
+
+        <section className='secondRow'>
+          <button type="button" className='btn btn-primary' id='btn-4' onClick={()=> numericBtn('4')}>4</button>
+          <button type="button" className='btn btn-primary' id='btn-5' onClick={()=> numericBtn('5')}>5</button>
+          <button type="button" className='btn btn-primary' id='btn-6' onClick={()=> numericBtn('6')}>6</button>
+        </section>
+
+        <section className='thirdRow'>
+          <button type="button" className='btn btn-primary' id='btn-7' onClick={()=> numericBtn('7')}>7</button>
+          <button type="button" className='btn btn-primary' id='btn-8' onClick={()=> numericBtn('8')}>8</button>
+          <button type="button" className='btn btn-primary' id='btn-9' onClick={()=> numericBtn('9')}>9</button>
+        </section>
+
+        <section className='fourthRow'>
+          <button type="button" className='btn btn-primary' id='btn-back'   onClick={()=> backBtn()}>←</button>
+          <button type="button" className='btn btn-primary' id='btn-0'      onClick={()=> numericBtn('0')}>0</button>
+          <button type="button" className='btn btn-primary' id='btn-check'  onClick={()=> checkBtn()}>✓</button>
+        </section>
+
       </header>
     </div>
   );
